366c68a59a2296f18052d7b8061c34cdaa1aebdf
HidalgoFernando <fh017049@gmail.com>
2023-11-10 14:55:34 -0300

    aa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.lectonfc"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.lectonfc"
--- app/src/main/java/com/example/lectonfc/MainActivity.java
+++ app/src/main/java/com/example/lectonfc/MainActivity.java
@@ -5,14 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textViewNFC;
     private NfcAdapter nfcAdapter;
 
     @Override
@@ -20,50 +18,81 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewNFC = findViewById(R.id.textViewNFC);
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         if (nfcAdapter == null) {
-            Toast.makeText(this, "NFC no es compatible con este dispositivo", Toast.LENGTH_SHORT).show();
+            // El dispositivo no soporta NFC
+            Toast.makeText(this, "Este dispositivo no tiene soporte para NFC", Toast.LENGTH_SHORT).show();
             finish();
             return;
         }
+
         if (!nfcAdapter.isEnabled()) {
-            Toast.makeText(this, "Activa NFC en tu dispositivo", Toast.LENGTH_SHORT).show();
-        }
-        handleIntent(getIntent());
-    }
-    private void handleIntent(Intent intent) {
-        String action = intent.getAction();
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            String uid = bytesToHexString(tag.getId());
-            textViewNFC.setText("UID: " + uid);
+            // El NFC no está habilitado
+            Toast.makeText(this, "Habilita el NFC en la configuración del dispositivo", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
         }
     }
-    private String bytesToHexString(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (byte aByte : bytes) {
-            sb.append(String.format("%02X", aByte));
-        }
-        return sb.toString();
-    }
+
     @Override
     protected void onResume() {
         super.onResume();
+
         Intent intent = new Intent(this, getClass());
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE);
-        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+
+        nfcAdapter.enableForegroundDispatch(
+                this,
+                pendingIntent,
+                null,
+                null
+        );
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         nfcAdapter.disableForegroundDispatch(this);
     }
+
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        handleIntent(intent);
+
+        String action = intent.getAction();
+        Log.d("NFC", "Acción del intent: " + action);
+
+        byte[] uidBytes = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+
+        if (uidBytes != null) {
+            String uid = ByteArrayToHexString(uidBytes);
+            Toast.makeText(this, "UID de la tarjeta: " + uid, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "No se encontraron datos de la tarjeta", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private String ByteArrayToHexString(byte[] array) {
+        if (array == null) {
+            return "No hay datos";
+        }
+
+        StringBuilder sb = new StringBuilder(array.length * 2);
+        for (byte b : array) {
+            int v = b & 0xFF;
+            if (v < 16) {
+                sb.append('0');
+            }
+            sb.append(Integer.toHexString(v));
+        }
+        return sb.toString().toUpperCase();
     }
 }
